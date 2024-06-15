5bd4ff28216da6ca22c839999a117756ca4eb3dd
Andi Florea <alexflorea16000@gmail.com>
2024-06-15 21:34:33 +0300

    Write function implementation

--- dbr-gpio-driver.c
+++ dbr-gpio-driver.c
@@ -6,9 +6,32 @@
 #include <linux/slab.h>
 
 #define DBR_MAX_USER_SIZE 1024
+#define BCM2711_GPIO_ADDRESS 0xfe200000
 
 static struct proc_dir_entry *dbr_proc = NULL;
 static char data_buffer[DBR_MAX_USER_SIZE];
+
+unsigned int *gpio_registers = NULL;
+
+static void gpio_pin_on(unsigned int pin) {
+  unsigned int fsel_index = pin/10;
+	unsigned int fsel_bitpos = pin%10;
+	unsigned int *gpio_fsel = gpio_registers + fsel_index;
+	unsigned int *gpio_on_register = (unsigned int*)((char*)gpio_registers + 0x1c);
+
+	*gpio_fsel &= ~(7 << (fsel_bitpos*3));
+	*gpio_fsel |= (1 << (fsel_bitpos*3));
+	*gpio_on_register |= (1 << pin);
+
+	return;
+}
+
+static void gpio_pin_off(unsigned int pin)
+{
+	unsigned int *gpio_off_register = (unsigned int*)((char*)gpio_registers + 0x28);
+	*gpio_off_register |= (1<<pin);
+	return;
+}
 
 ssize_t dbr_read(struct file *file, char __user *user, size_t size, loff_t *off) {
   copy_to_user(user, "Data from kernel...\n", 20);
@@ -16,16 +39,49 @@ ssize_t dbr_read(struct file *file, char __user *user, size_t size, loff_t *off)
 }
 
 ssize_t dbr_write(struct file *file, const char __user *user, size_t size, loff_t *off) {
-  memset(data_buffer, 0x0, sizeof(data_buffer));
+	unsigned int pin = UINT_MAX;
+	unsigned int value = UINT_MAX;
 
-  if (size > DBR_MAX_USER_SIZE) {
-    size = DBR_MAX_USER_SIZE;
-  }
+	memset(data_buffer, 0x0, sizeof(data_buffer));
 
-  copy_from_user(data_buffer, user, size);
+	if (size > DBR_MAX_USER_SIZE)
+	{
+		size = DBR_MAX_USER_SIZE;
+	}
 
-  printk("GPIO: Received '%s'\n", data_buffer);
-  return size;
+	if (copy_from_user(data_buffer, user, size))
+		return 0;
+
+	printk("GPIO: Data buffer: %s\n", data_buffer);
+
+	if (sscanf(data_buffer, "%d,%d", &pin, &value) != 2)
+	{
+		printk("GPIO: Invalid data format!\n");
+		return size;
+	}
+
+	if (pin > 21 || pin < 0)
+	{
+		printk("GPIO: Invalid pin number\n");
+		return size;
+	}
+
+	if (value != 0 && value != 1)
+	{
+		printk("GPIO: Invalid on/off value\n");
+		return size;
+	}
+
+	printk("GPIO: Setting pin %d to value %d\n", pin, value);
+	if (value == 1)
+	{
+		gpio_pin_on(pin);
+	} else if (value == 0)
+	{
+		gpio_pin_off(pin);
+	}
+
+	return size;
 }
 
 static const struct proc_ops dbr_proc_fops = 
@@ -35,18 +91,29 @@ static const struct proc_ops dbr_proc_fops =
 };
 
 static int __init gpio_driver_init(void) {
-  printk("GPIO: Initializing driver...\n");
+	printk("GPIO: Initializing driver...\n");
+	
+	gpio_registers = (int*)ioremap(BCM2711_GPIO_ADDRESS, PAGE_SIZE);
+	if (gpio_registers == NULL)
+	{
+		printk("GPIO: Failed to map GPIO memory to driver\n");
+		return -1;
+	}
+	
+	printk("GPIO: Successfully mapped in GPIO memory\n");
+	
+	dbr_proc = proc_create("dbr-gpio", 0666, NULL, &lll_proc_fops);
+	if (dbr_proc == NULL)
+	{
+		return -1;
+	}
 
-  dbr_proc = proc_create("dbr-gpio", 0666, NULL, &dbr_proc_fops);
-  if (dbr_proc == NULL) {
-    return -1;
-  }
-
-  return 0;
+	return 0;
 }
 
 static void __exit gpio_driver_exit(void) {
   printk("GPIO: Exiting driver...");
+  iounmap(gpio_registers);
   proc_remove(dbr_proc);
   return;
 }
